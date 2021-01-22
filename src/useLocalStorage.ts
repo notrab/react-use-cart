9f2cb2508633bff910c5804be44ca26990699d75
getTobiasNielsen <54803528+getTobiasNielsen@users.noreply.github.com>
2021-01-22 16:57:28 +0100

    Update src/useLocalStorage.ts
    Co-authored-by: Jamie Barton <jamie@notrab.dev>

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import * as React from "react";
 
 export default function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = React.useState(() => {
