4824d8b1a2f7bacf3c3b8692c575ab7226c2b168
Jamie Barton <jamie@notrab.dev>
2021-02-03 16:30:39 +0000

    feat: typescript (#56)
    * feat: add typescript (#53)
    
    * Update prettierrc.config.js
    
    I did this to make the files more consistent
    
    * Revert "Update prettierrc.config.js"
    
    This reverts commit a48336d0a97fff10000d86faba6b9bf812df1faf.
    
    * Update prettierrc.config.js
    
    * Renamed prettier config properly
    
    https://prettier.io/docs/en/configuration.html
    
    * Hit the most important files with prettier, to avoid mixing prettier corrections with real changes later
    
    * Added typescript, and also a very open tsconfig
    
    This should lay a foundation for future update, but they should be done incrementally
    
    * Added ts supprt and fixed a type declaration path mistake
    
    Updated jest, added babel typescript for jest, added a type to CartProvider
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update src/useLocalStorage.ts
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update src/useLocalStorage.ts
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Added some of the updates
    
    * Added * as React
    
    * Added some interfaces, some strictness, some looseness
    
    * Fixed typo
    
    * Added optional on the CartProvider props
    
    * Missed an any, will need a proper type
    
    * Added payload, instead of item
    
    * Fixed typo
    
    * Turned on  "noImplicitAny": true,  "strictNullChecks": true, and "forceConsistentCasingInFileNames": true,
    
    * Changed the last flags to true. TS is fully strict now.
    
    * Stronger typed CartProvider props
    
    * Little stronger storage type
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * chore: update release config (#54) (#55)
    
    * chore(pkg): update keywords
    
    Co-authored-by: getTobiasNielsen <54803528+getTobiasNielsen@users.noreply.github.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
+import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 
 import pkg from "./package.json";
 
 export default {
-  input: "index.js",
+  input: "src/index.tsx",
   output: [
     {
       file: pkg.main,
@@ -25,6 +26,7 @@ export default {
     babel({
       exclude: "node_modules/**",
     }),
+    typescript(),
     commonjs(),
   ],
 };
