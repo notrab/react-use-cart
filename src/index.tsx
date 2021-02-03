f904674db5fb2d1a2436199ef8287ca4a0bf0e63
getTobiasNielsen <54803528+getTobiasNielsen@users.noreply.github.com>
2021-02-03 17:17:02 +0100

    feat: add typescript (#53)
    * Update prettierrc.config.js
    
    I did this to make the files more consistent
    
    * Revert "Update prettierrc.config.js"
    
    This reverts commit a48336d0a97fff10000d86faba6b9bf812df1faf.
    
    * Update prettierrc.config.js
    
    * Renamed prettier config properly
    
    https://prettier.io/docs/en/configuration.html
    
    * Hit the most important files with prettier, to avoid mixing prettier corrections with real changes later
    
    * Added typescript, and also a very open tsconfig
    
    This should lay a foundation for future update, but they should be done incrementally
    
    * Added ts supprt and fixed a type declaration path mistake
    
    Updated jest, added babel typescript for jest, added a type to CartProvider
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update prettier.config.js
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update src/useLocalStorage.ts
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Update src/useLocalStorage.ts
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>
    
    * Added some of the updates
    
    * Added * as React
    
    * Added some interfaces, some strictness, some looseness
    
    * Fixed typo
    
    * Added optional on the CartProvider props
    
    * Missed an any, will need a proper type
    
    * Added payload, instead of item
    
    * Fixed typo
    
    * Turned on  "noImplicitAny": true,  "strictNullChecks": true, and "forceConsistentCasingInFileNames": true,
    
    * Changed the last flags to true. TS is fully strict now.
    
    * Stronger typed CartProvider props
    
    * Little stronger storage type
    
    Co-authored-by: Jamie Barton <jamie@notrab.dev>

---
+++ src/index.tsx
@@ -0,0 +1,272 @@
+import * as React from "react";
+
+import useLocalStorage from "./useLocalStorage";
+
+// Interfaces:
+interface Item {
+  id: string;
+  price: number;
+  quantity?: number;
+  itemTotal?: number;
+  [key: string]: any;
+}
+
+interface InitialState {
+  items: Item[];
+  isEmpty: boolean;
+  totalItems: number;
+  totalUniqueItems: number;
+  cartTotal: number;
+}
+
+interface CartProviderState extends InitialState {
+  addItem: (item: Item, quantity?: number) => void;
+  removeItem: (id: Item["id"]) => void;
+  updateItem: (id: Item["id"], payload: object) => void;
+  updateItemQuantity: (id: Item["id"], quantity: number) => void;
+  emptyCart: () => void;
+  getItem: (id: Item["id"]) => any | undefined;
+  inCart: (id: Item["id"]) => boolean;
+}
+
+export type Actions =
+  | { type: "SET_ITEMS"; payload: Item[] }
+  | { type: "ADD_ITEM"; payload: Item }
+  | { type: "REMOVE_ITEM"; id: Item["id"] }
+  | {
+      type: "UPDATE_ITEM";
+      id: Item["id"];
+      payload: object;
+    }
+  | { type: "EMPTY_CART" };
+
+export const initialState: any = {
+  items: [],
+  isEmpty: true,
+  totalItems: 0,
+  totalUniqueItems: 0,
+  cartTotal: 0,
+};
+
+const CartContext = React.createContext<CartProviderState | undefined>(
+  initialState
+);
+
+export const createCartIdentifier = (len = 12) =>
+  [...Array(len)].map(() => (~~(Math.random() * 36)).toString(36)).join("");
+
+export const useCart = () => {
+  // This makes sure that the cart functions are always defined before calling it.
+  const context = React.useContext(CartContext);
+  if (!context) throw new Error("Expected to be wrapped in a CartProvider");
+  return context;
+};
+
+function reducer(state: CartProviderState, action: Actions) {
+  switch (action.type) {
+    case "SET_ITEMS":
+      return generateCartState(state, action.payload);
+
+    case "ADD_ITEM": {
+      const items = [...state.items, action.payload];
+
+      return generateCartState(state, items);
+    }
+
+    case "UPDATE_ITEM": {
+      const items = state.items.map((item: Item) => {
+        if (item.id !== action.id) return item;
+
+        return {
+          ...item,
+          ...action.payload,
+        };
+      });
+
+      return generateCartState(state, items);
+    }
+
+    case "REMOVE_ITEM": {
+      const items = state.items.filter((i: Item) => i.id !== action.id);
+
+      return generateCartState(state, items);
+    }
+
+    case "EMPTY_CART":
+      return initialState;
+
+    default:
+      throw new Error("No action specified");
+  }
+}
+
+const generateCartState = (state = initialState, items: Item[]) => {
+  const totalUniqueItems = calculateUniqueItems(items);
+  const isEmpty = totalUniqueItems === 0;
+
+  return {
+    ...initialState,
+    ...state,
+    items: calculateItemTotals(items),
+    totalItems: calculateTotalItems(items),
+    totalUniqueItems,
+    cartTotal: calculateCartTotal(items),
+    isEmpty,
+  };
+};
+
+const calculateItemTotals = (items: Item[]) =>
+  items.map((item) => ({
+    itemTotal: item.price * item.quantity!,
+    ...item,
+  }));
+
+const calculateCartTotal = (items: Item[]) =>
+  items.reduce((total, item) => total + item.quantity! * item.price, 0);
+
+const calculateTotalItems = (items: Item[]) =>
+  items.reduce((sum, item) => sum + item.quantity!, 0);
+
+const calculateUniqueItems = (items: Item[]) => items.length;
+
+export const CartProvider: React.FC<{
+  children?: React.ReactNode;
+  id?: string;
+  defaultItems?: Item[];
+  onSetItems?: (items: Item[]) => void;
+  onItemAdd?: (payload: Item) => void;
+  onItemUpdate?: (payload: object) => void;
+  onItemRemove?: (id: Item["id"]) => void;
+  storage?: (
+    key: string,
+    initialValue: string
+  ) => [string, (value: Function | string) => void];
+}> = ({
+  children,
+  id: cartId,
+  defaultItems = [],
+  onSetItems,
+  onItemAdd,
+  onItemUpdate,
+  onItemRemove,
+  storage = useLocalStorage,
+}) => {
+  const id = cartId ? cartId : createCartIdentifier();
+
+  const [savedCart, saveCart] = storage(
+    cartId ? `react-use-cart-${id}` : `react-use-cart`,
+    JSON.stringify({
+      id,
+      ...initialState,
+      items: defaultItems,
+    })
+  );
+
+  const [state, dispatch] = React.useReducer(reducer, JSON.parse(savedCart));
+  React.useEffect(() => {
+    saveCart(JSON.stringify(state));
+  }, [state, saveCart]);
+
+  const setItems = (items: Item[]) => {
+    dispatch({
+      type: "SET_ITEMS",
+      payload: items,
+    });
+
+    onSetItems && onSetItems(items);
+  };
+
+  const addItem = (item: Item, quantity = 1) => {
+    if (quantity <= 0) return;
+    if (!item.id) throw new Error("You must provide an `id` for items");
+
+    const currentItem = state.items.find((i: Item) => i.id === item.id);
+
+    if (!currentItem && !item.hasOwnProperty("price"))
+      throw new Error("You must pass a `price` for new items");
+
+    if (!currentItem) {
+      const payload = { ...item, quantity };
+
+      dispatch({ type: "ADD_ITEM", payload });
+
+      onItemAdd && onItemAdd(payload);
+
+      return;
+    }
+
+    const payload = { ...item, quantity: currentItem.quantity + quantity };
+
+    dispatch({
+      type: "UPDATE_ITEM",
+      id: item.id,
+      payload,
+    });
+
+    onItemUpdate && onItemUpdate(payload);
+  };
+
+  const updateItem = (id: Item["id"], payload: object) => {
+    dispatch({ type: "UPDATE_ITEM", id, payload });
+
+    onItemUpdate && onItemUpdate(payload);
+  };
+
+  const updateItemQuantity = (id: Item["id"], quantity: number) => {
+    if (quantity <= 0) {
+      onItemRemove && onItemRemove(id);
+
+      dispatch({ type: "REMOVE_ITEM", id });
+
+      return;
+    }
+
+    const currentItem = state.items.find((item: Item) => item.id === id);
+
+    if (!currentItem) throw new Error("No such item to update");
+
+    const payload = { ...currentItem, quantity };
+
+    dispatch({
+      type: "UPDATE_ITEM",
+      id,
+      payload,
+    });
+
+    onItemUpdate && onItemUpdate(payload);
+  };
+
+  const removeItem = (id: Item["id"]) => {
+    dispatch({ type: "REMOVE_ITEM", id });
+
+    onItemRemove && onItemRemove(id);
+  };
+
+  const emptyCart = () =>
+    dispatch({
+      type: "EMPTY_CART",
+    });
+
+  const getItem = (id: Item["id"]) =>
+    state.items.find((i: Item) => i.id === id);
+
+  const inCart = (id: Item["id"]) => state.items.some((i: Item) => i.id === id);
+
+  return (
+    <CartContext.Provider
+      value={{
+        ...state,
+        getItem,
+        inCart,
+        setItems,
+        addItem,
+        updateItem,
+        updateItemQuantity,
+        removeItem,
+        emptyCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
